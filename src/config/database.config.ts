a827e4ca534899abb80999be41a444a2e4d549d3
Giwan Kim <giwan.kim@gmail.com>
2023-09-16 13:05:56 +0900

    fix(docker): build stage includes dev dependencies

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,7 +4,7 @@ import * as process from 'process';
 export default registerAs('database', () => {
   return {
     host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.DB_PORT ?? '5432', 10),
+    port: parseInt(process.env.POSTGRES_PORT ?? '5432', 10),
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
